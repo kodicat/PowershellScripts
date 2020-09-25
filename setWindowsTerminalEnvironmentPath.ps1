c71cb21cf7d3f45f36774e34486b8cf31516a0a5
Ievgenii Iukhymchuk <ievgenii_iukhymchuk@epam.com>
2020-09-25 23:14:15 +0300

    Change the update wt path script

--- setWindowsTerminalEnvironmentPath.ps1
+++ setWindowsTerminalEnvironmentPath.ps1
@@ -1,6 +1,6 @@
 $winApps = "c:\Program Files\WindowsApps\";
 $wtEnvVariable = "WT_Path";
-$wtName = (Get-ChildItem -Path $winApps -Filter "Microsoft.WindowsTerminal_*__8wekyb3d8bbwe")[0].Name;
+$wtName = (Get-ChildItem -Path $winApps -Filter "Microsoft.WindowsTerminal_*__8wekyb3d8bbwe" | Sort-Object -Property Name -Descending)[0];
 if ($wtName) {
 	[System.Environment]::SetEnvironmentVariable($wtEnvVariable, $winApps + $wtName, [System.EnvironmentVariableTarget]::User);
 }
